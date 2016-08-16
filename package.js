657f8c6172145f6de796b11e20ad30680bd947a4
honwlee <my.dream.gardens@gmail.com>
2016-08-16 09:08:40 +0900

    Create package.js

---
+++ package.js
@@ -0,0 +1,30 @@
+{
+  "name": "todo",
+  "version": "1.0.0",
+  "main": "app/index.html",
+  "scripts": {
+    "test": "echo \"TodoList\" && exit 1"
+  },
+  "author": "honwlee",
+  "license": "MIT",
+  "description": "react sample app TodoList",
+  "dependencies": {
+    "babelify": "^7.3.0",
+    "browserify": "^13.1.0",
+    "eventemitter": "^0.3.3",
+    "gulp": "^3.9.1",
+    "gulp-jasmine": "^2.4.0",
+    "run-sequence": "^1.2.2",
+    "vinyl-source-stream": "^1.1.0"
+  },
+  "devDependencies": {
+    "babel-preset-es2015": "^6.13.2",
+    "babel-preset-react": "^6.11.1",
+    "babelify": "^7.3.0",
+    "gulp": "^3.9.1",
+    "gulp-cli": "^1.2.2",
+    "gulp-react": "^3.1.0",
+    "react": "^15.3.0",
+    "react-dom": "^15.3.0"
+  }
+}
